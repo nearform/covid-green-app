177ef1934800b54dac2babf2fcfd0d7e6344c328
Andrea Forni <andrea.forni@nearform.com>
2020-10-20 16:51:27 +0200

    fix: update the qr code parser to parse the new URL inside the QR  code.

--- venue-check-in/src/services/constants.ts
+++ venue-check-in/src/services/constants.ts
@@ -1,3 +1,3 @@
-export const QR_CODE_FIELD_DELIMITER = ':';
-export const QR_CODE_CONSTANT = 'DEMOQRCODETRACINGAPP';
 export const QR_CODE_MIN_VERSION = 1;
+export const QR_CODE_URL = 'https://covidtracker.gov.ie/';
+export const QR_CODE_JWT_URL_PARAM = 'content';
--- venue-check-in/src/services/qr-code-parser.ts
+++ venue-check-in/src/services/qr-code-parser.ts
@@ -1,21 +1,26 @@
-import {
-  QR_CODE_CONSTANT,
-  QR_CODE_FIELD_DELIMITER,
-  QR_CODE_MIN_VERSION
-} from './constants';
+import {QR_CODE_JWT_URL_PARAM, QR_CODE_MIN_VERSION} from './constants';
 import jwt_decode from 'jwt-decode';
 import jsrsasign from 'jsrsasign';
 import {publicKey} from '../../qr-public.pem';
+import {QR_CODE_URL} from './constants';
 
 interface ParsedVenue {
   iat: string;
   id: string;
+  version: number;
   venueAddress: string;
   venueName: string;
 }
 
-const isVersionSupported = (version: string) =>
-  parseInt(version, 10) >= QR_CODE_MIN_VERSION;
+const getJWT = (url: string): string => {
+  const matches = url.match(`${QR_CODE_JWT_URL_PARAM}=([^&]*)`);
+  if (matches === null || matches[1] === null) {
+    throw 'Invalid QR code value';
+  }
+  const jwt = matches[1];
+
+  return jwt;
+};
 
 const parseJWT = async (jwt: string): Promise<ParsedVenue> => {
   const parts = jwt.split('.');
@@ -36,24 +41,23 @@ const verifySignature = async (jwtString: string) => {
   }
 };
 
-export const parseQRCode = async (value: string): Promise<ParsedVenue> => {
-  const fields = value.split(QR_CODE_FIELD_DELIMITER);
-  if (fields.length !== 3) {
-    throw 'Invalid QR code value';
+const isVersionSupported = (jwt: ParsedVenue) =>
+  jwt.version >= QR_CODE_MIN_VERSION;
+
+export const parseQRCode = async (url: string): Promise<ParsedVenue> => {
+  if (!url.startsWith(QR_CODE_URL)) {
+    throw 'Invalid QR code value 1';
   }
 
-  const [constant, version, jwtString] = fields;
-  if (constant !== QR_CODE_CONSTANT) {
-    throw 'Invalid QR code value';
-  }
+  const jwt = getJWT(url);
 
-  if (!isVersionSupported(version)) {
+  const jwtPayload = await parseJWT(jwt);
+
+  await verifySignature(jwt);
+
+  if (!isVersionSupported(jwtPayload)) {
     throw 'Invalid version';
   }
-
-  const jwtPayload = await parseJWT(jwtString);
-
-  await verifySignature(jwtString);
 
   return jwtPayload;
 };
