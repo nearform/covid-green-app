424c2a1ca64e47fe800bd4e078383d97a968e7fa
Andrea Forni <andrea.forni@nearform.com>
2020-10-21 10:43:28 +0200

    fix: update URL in QR code's payload

--- venue-check-in/src/services/constants.ts
+++ venue-check-in/src/services/constants.ts
@@ -1,3 +1,3 @@
 export const QR_CODE_MIN_VERSION = 1;
-export const QR_CODE_URL = 'https://covidtracker.gov.ie/';
+export const QR_CODE_URL = 'https://pg-qr-demo.nf-covid-services.com/scan';
 export const QR_CODE_JWT_URL_PARAM = 'content';
